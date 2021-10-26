3d41ef8d7a5e11c39442ca50d8968a15c0f35e8a
Sidra Sattar <sidrasattar@geniusmindzone.com>
2021-10-27 00:25:24 +0500

    build remove

--- build/app/Controllers/Http/base/CrudsController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CrudsController {
-}
-exports.default = CrudsController;
-//# sourceMappingURL=CrudsController.js.map
