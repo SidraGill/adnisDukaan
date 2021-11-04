72c7f963c14eeb46e6385489f671b0814303a808
Sidra Sattar <sidrasattar@geniusmindzone.com>
2021-11-04 23:56:16 +0500

    del build

--- build/app/Controllers/Http/admin/AuthController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AuthController {
-}
-exports.default = AuthController;
-//# sourceMappingURL=AuthController.js.map
